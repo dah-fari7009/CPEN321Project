af4946cb17fbf5e9814627042a55b1332318cfec
Ali Javed Ansari <alijavedansari@gmail.com>
2022-06-26 15:55:16 -0400

    Finalize Teammates Screen - gave buttons some basic functionality

--- FE/app/src/main/java/com/example/dodged_project/FinalizeTeammatesActivity.java
+++ FE/app/src/main/java/com/example/dodged_project/FinalizeTeammatesActivity.java
@@ -2,13 +2,27 @@ package com.example.dodged_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FinalizeTeammatesActivity extends AppCompatActivity {
 
+    private Button addTeammatesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finalize_teammates);
+
+        addTeammatesButton = findViewById(R.id.add_teammates_button);
+
+        addTeammatesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addTeammatesIntent = new Intent(FinalizeTeammatesActivity.this, AddTeammatesActivity.class);
+                startActivity(addTeammatesIntent);
+            }
+        });
     }
 }
