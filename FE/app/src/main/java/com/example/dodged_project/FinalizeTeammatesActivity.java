5b06604ea8167901768edd1d7920b946602f3c8b
Ali Javed Ansari <alijavedansari@gmail.com>
2022-06-26 15:38:03 -0400

    Started Finalize Teammates Screen

---
+++ FE/app/src/main/java/com/example/dodged_project/FinalizeTeammatesActivity.java
@@ -0,0 +1,14 @@
+package com.example.dodged_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinalizeTeammatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finalize_teammates);
+    }
+}
