4534dfc9cca863f7c0ef6ca2092b88a2fbf10745
Ali Javed Ansari <alijavedansari@gmail.com>
2022-07-02 14:58:09 -0400

    UI done, finishing up add teammates functionality soon

--- FE/app/src/main/java/com/example/dodged_project/AddTeammatesActivity.java
+++ FE/app/src/main/java/com/example/dodged_project/AddTeammatesActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class AddTeammatesActivity extends AppCompatActivity {
 
     private Button confirmButton;
+    private Button cancelButton;
+    private ImageView xButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,8 @@ public class AddTeammatesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_teammates);
 
         confirmButton = findViewById(R.id.confirm_button);
+        cancelButton = findViewById(R.id.cancel_button);
+        xButton = findViewById(R.id.x_button_close);
 
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +52,22 @@ public class AddTeammatesActivity extends AppCompatActivity {
                 Intent finalizeTeammatesIntent = new Intent(AddTeammatesActivity.this, FinalizeTeammatesActivity.class);
                 finalizeTeammatesIntent.putExtra("user_input_playernames", usernames);
                 startActivity(finalizeTeammatesIntent);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cancelFromAddTeammatesActivityIntent = new Intent(AddTeammatesActivity.this, ChooseTeammatesActivity.class);
+                startActivity(cancelFromAddTeammatesActivityIntent);
+            }
+        });
+
+        xButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cancelFromAddTeammatesActivityIntent = new Intent(AddTeammatesActivity.this, ChooseTeammatesActivity.class);
+                startActivity(cancelFromAddTeammatesActivityIntent);
             }
         });
     }
