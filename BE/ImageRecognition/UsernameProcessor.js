86d122f7b621f8faaa29ebd0f331adef3e2052d6
Justin Cho <justincho@dhcp-128-189-247-59.ubcsecure.wireless.ubc.ca>
2022-07-05 19:17:22 -0700

    modify username processor

--- BE/ImageRecognition/UsernameProcessor.js
+++ BE/ImageRecognition/UsernameProcessor.js
@@ -1,36 +1,68 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient({
-    keyFilename: '<--- GOOGLE VISION API KEY JSON FILE --->'
+    keyFilename: 'vision-key.json'
 });
+const stats = require('../Prediction/TeamStats')
 
 const JOINED_THE_LOBBY = " joined the lobby"
 
 // Upload riot ids
 router.post('/usernames', (req, res) => {
-    var ids = {
-        id1 : req.body.id1,
-        id2 : req.body.id2,
-        id3 : req.body.id3,
-        id4 : req.body.id4,
-        id5 : req.body.id5,
+    const ids = []
+    ids[0] = req.body.id1
+    ids[1] = req.body.id2
+    ids[2] = req.body.id3
+    ids[3] = req.body.id4
+    ids[4] = req.body.id5
+
+    const region = req.body.region
+
+    if (!region) {
+        res.status(400).json("Region must be specified")
     }
 
-    if (!ids.id1 || !ids.id2 || !ids.id3 || !ids.id4 || !ids.id5) {
+    if (!ids[0] || !ids[1] || !ids[2] || !ids[3] || !ids[4]) {
         res.status(400).json("All riot ids must be specified")
     } else {
-        // TODO: send riot ids to team stats class 
-        res.status(200)
+        // TODO: send riot ids to team stats
+        res.status(200).json(ids)
     }
 })
 
-// Upload image for further processing
+// Upload image to parse riot ids
 router.get('/', async (req, res) => {
-    // TODO: get image from frontend
-    parseText('<--- FILE NAME --->').then(() => {
-        // TODO: send riot ids to team stats class
-        res.status(200)
+    const base64EncodedImage = req.base64EncodedImage
+    const data = Buffer.from(base64EncodedImage, 'base64')
+    const region = req.body.region
+
+    if (!region) {
+        res.status(400).json("Region must be specified")
+        return
+    }
+    
+    fs.writeFile('usernames.png', data, (err) => {
+        if (err) {
+            res.status(500).json(err)
+            return
+        } else {
+            console.log('successfully wrote file')
+        }
+    })
+
+    await parseText('usernames.png').then((ids) => {
+        // TODO: send riot ids to team stats and return response
+        console.log(ids)
+    })
+
+    fs.unlink('usernames.png', (err) => {
+        if (err) {
+            res.status(500).json(err)
+        } else {
+            console.log('successfully deleted file')
+        }
     })
 })
 
@@ -49,7 +81,7 @@ async function parseText(fileName) {
         }
     });
 
-    console.log(ids)
+    return ids
 }
 
 module.exports = router
