ee407b8a1d94ffa06d36f4d2175c4ecee8c060aa
Ali Javed Ansari <alijavedansari@gmail.com>
2022-07-18 19:10:45 -0400

    Removed unused imports

--- FE/app/src/androidTest/java/com/example/dodged_project/MainActivityTest.java
+++ FE/app/src/androidTest/java/com/example/dodged_project/MainActivityTest.java
@@ -5,15 +5,9 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.assertEquals;
 
-import android.content.Context;
-
-import androidx.test.espresso.ViewAssertion;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Rule;
 import org.junit.Test;
