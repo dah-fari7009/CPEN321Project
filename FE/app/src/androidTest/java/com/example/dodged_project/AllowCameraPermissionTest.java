e1cc950d9b32454d587ad6f5c138c010f9d31ae4
Ali Javed Ansari <alijavedansari@gmail.com>
2022-07-28 23:19:32 -0400

    FE test WIP

---
+++ FE/app/src/androidTest/java/com/example/dodged_project/AllowCameraPermissionTest.java
@@ -0,0 +1,94 @@
+//package com.example.dodged_project;
+//
+//
+//import static androidx.test.espresso.Espresso.onView;
+//import static androidx.test.espresso.action.ViewActions.click;
+//import static androidx.test.espresso.assertion.ViewAssertions.matches;
+//import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+//import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+//import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+//import static androidx.test.espresso.matcher.ViewMatchers.withId;
+//import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+//import static androidx.test.espresso.matcher.ViewMatchers.withText;
+//import static org.hamcrest.Matchers.allOf;
+//import static org.hamcrest.Matchers.is;
+//
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.view.ViewParent;
+//
+//import androidx.test.espresso.ViewInteraction;
+//import androidx.test.ext.junit.rules.ActivityScenarioRule;
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
+//import androidx.test.filters.LargeTest;
+//import androidx.test.rule.GrantPermissionRule;
+//
+//import org.hamcrest.Description;
+//import org.hamcrest.Matcher;
+//import org.hamcrest.TypeSafeMatcher;
+//import org.junit.Rule;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//@LargeTest
+//@RunWith(AndroidJUnit4.class)
+//public class AllowCameraPermissionTest {
+//
+//    @Rule
+//    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+//            new ActivityScenarioRule<>(MainActivity.class);
+//
+//    @Rule
+//    public GrantPermissionRule mGrantPermissionRule =
+//            GrantPermissionRule.grant(
+//                    "android.permission.CAMERA");
+//
+//    @Test
+//    public void allowCameraPermissionTest() {
+//        ViewInteraction materialButton = onView(
+//                allOf(withId(R.id.continue_as_guest_button), withText("Continue as Guest"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                3),
+//                        isDisplayed()));
+//        materialButton.perform(click());
+//
+//        ViewInteraction materialButton2 = onView(
+//                allOf(withId(R.id.upload_photo_button), withText("Upload Photo"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withClassName(is("android.widget.LinearLayout")),
+//                                        1),
+//                                1),
+//                        isDisplayed()));
+//        materialButton2.perform(click());
+//
+//        ViewInteraction imageView = onView(
+//                allOf(withId(com.android.camera2.R.id.shutter_button), withContentDescription("Shutter"),
+//                        withParent(allOf(withId(com.android.camera2.R.id.bottombar_capture),
+//                                withParent(withId(com.android.camera2.R.id.bottom_bar)))),
+//                        isDisplayed()));
+//        imageView.check(matches(isDisplayed()));
+//    }
+//
+//    private static Matcher<View> childAtPosition(
+//            final Matcher<View> parentMatcher, final int position) {
+//
+//        return new TypeSafeMatcher<View>() {
+//            @Override
+//            public void describeTo(Description description) {
+//                description.appendText("Child at position " + position + " in parent ");
+//                parentMatcher.describeTo(description);
+//            }
+//
+//            @Override
+//            public boolean matchesSafely(View view) {
+//                ViewParent parent = view.getParent();
+//                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+//                        && view.equals(((ViewGroup) parent).getChildAt(position));
+//            }
+//        };
+//    }
+//}
