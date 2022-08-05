a90e10ec3eae2518d2138d19bf6c9dbf905d781c
Ali Javed Ansari <alijavedansari@gmail.com>
2022-08-05 11:35:41 -0400

    removed unused front end imports

--- FE/app/src/main/java/com/example/dodged_project/AddTeammatesActivity.java
+++ FE/app/src/main/java/com/example/dodged_project/AddTeammatesActivity.java
@@ -1,13 +1,13 @@
 package com.example.dodged_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.dodged_project.databinding.ActivityAddTeammatesBinding;
 
